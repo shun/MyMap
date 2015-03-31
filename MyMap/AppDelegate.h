a247e7465f8a69757e1ab4aaadfd9bc540015512
Shunsuke Kudo <skudo_xx@yahoo.co.jp>
2015-03-31 22:57:19 +0900

    Initial Commit

---
+++ MyMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMap
+//
+//  Created by KudoShunsuke on 3/31/15.
+//  Copyright (c) 2015 KudoShunsuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMap
+//
+//  Created by KudoShunsuke on 3/31/15.
+//  Copyright (c) 2015 KudoShunsuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
